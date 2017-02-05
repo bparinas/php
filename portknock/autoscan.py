a821a9d4778e43eb0e2f75bd8a75265dd02af360
bparinas <bparinas@hotmail.com>
2017-02-05 18:16:11 +0000

    added portknock

---
+++ portknock/autoscan.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import nmap
+res = open('tmp.csv','w')
+host = open('hosts.txt', 'r')
+nm = nmap.PortScanner()
+for target in host:
+  nm.scan(target, '1-65535')
+  res.write(nm.csv())
+host.close()
+res.close()
+
+#remove the header in csv and save it to a new file
+with open('tmp.csv') as old, open('scanout.csv', 'w') as new:
+   for line in old:
+      if 'extrainfo' not in line:
+         new.write(line)
+old.close()
+new.close()
---
+++ portknock/report.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>Port Scan Report</title>
+</head>
+<body>
+ <h1>Network Port Scan Report</h1>
+ <?php echo "Last modified: ".date("F d Y H:i:s.",filemtime("report.php")); ?>
+ <br><br>
+ <div>
+ <?php
+  print <<< HERE
+  <table border = "1">
+  <tr>
+   <th>Host</th>
+   <th>Hostname</th>
+   <th>Protocol</th>
+   <th>Port</th>
+   <th>Service</th>
+   <th>State</th>
+   <th>Product</th>
+   <th>Version</th>
+  </tr>
+HERE;
+
+//<?php
+$row = 1;
+
+if (($handle = fopen("scanout.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, ",")) !== FALSE) {
+        //if($row == 1){ $row++; continue; } // delete the first line
+        $num = count($data);
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+              $col = explode(";",$data[$c]);
+              echo "<tr>";
+              echo "<td>" . $col[0] . "</td>";
+              echo "<td>" . $col[1] . "</td>";
+              echo "<td>" . $col[3] . "</td>";
+              echo "<td>" . $col[4] . "</td>";
+              echo "<td>" . $col[5] . "</td>";
+              echo "<td>" . $col[6] . "</td>";
+              echo "<td>" . $col[7] . "</td>";
+              echo "<td>" . $col[10] . "</td>";
+              echo "</tr>";
+        }
+    }
+    fclose($handle);
+}
+
+  print "</table>";
+?>
+</div>
+</body>
+</html>
